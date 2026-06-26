8ccf9bf5ddc2846118d72efdcc7827bffd4b5b11
Mayank <realmayanknarang@gmail.com>
2026-06-26 12:20:41 +0530

    Update Mail.js

--- backend/config/Mail.js
+++ backend/config/Mail.js
@@ -1,24 +1,23 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 dotenv.config()
-
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  service: "Gmail",
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
-    user: process.env.BREVO_USER,   // your brevo login email
-    pass: process.env.BREVO_PASS,   // brevo SMTP key (not your password)
+    user:process.env.EMAIL,
+    pass:process.env.EMAIL_PASS,
   },
-})
+});
 
-const sendMail = async (to, otp) => {
-  await transporter.sendMail({
-    from: process.env.BREVO_USER,
+const sendMail=async (to,otp)=>{
+await transporter.sendMail({
+    from:`${process.env.EMAIL}`,
     to,
     subject: "Reset Your Password",
-    html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
-  })
+    html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
+})
 }
 
 export default sendMail
